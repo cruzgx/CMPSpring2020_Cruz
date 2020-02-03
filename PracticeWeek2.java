45c90c256482690e9b4e9a588dbe0ac6834b87fb
cruzgxx0 <60441169+cruzgxx0@users.noreply.github.com>
2020-02-03 12:41:03 -0500

    Add files via upload

---
+++ PracticeWeek2.java
@@ -0,0 +1,32 @@
+package practiceW2;
+/*
+ *  @author Cruz
+ *  created: 2/3/20
+ *  description: Variable Practice
+ */
+
+public class PracticeWeek2 {
+		
+			public static void main(String[] args) {
+				//Define a variable of type double with value 10.35
+				 double change = 10.35;
+				 byte first = 5;
+				 int place = 3;
+				
+				//Declare 3 string variables
+				 String wordOutput;
+				 String message;
+				 String instruction;
+				
+				//Assign values to the those previously created variables
+				wordOutput = "Sucess";
+				message = "Hello Word";
+				instruction = "Add 1 to the given number";	
+				
+				System.out.println(wordOutput);
+				System.out.println(wordOutput + " " + instruction);
+			}
+			
+	}
+
+
