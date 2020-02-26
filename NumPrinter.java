205242869fd3cb5932891412993c2de146801e59
cruzgxx0 <60441169+cruzgxx0@users.noreply.github.com>
2020-02-26 12:34:47 -0500

    Add files via upload

---
+++ NumPrinter.java
@@ -0,0 +1,17 @@
+/*
+ * @author Cruz
+ * Purpose: Prints all numbers from 5 to 99
+ * 
+ */
+public class NumPrinter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i = 5; i < 100; i++) {
+			System.out.printf("" + i + "\n");
+		
+		}
+	}
+
+}
